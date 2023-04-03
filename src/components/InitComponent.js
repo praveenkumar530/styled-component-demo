d47d9aa3aac236848a2327ae0ebfd0ba85a39bdb
Praveen Kumar <praveen.s@leadsquared.com>
2023-04-03 22:34:07 +0530

    format change

--- src/components/InitComponent.js
+++ src/components/InitComponent.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import StyledSuccessButton from './StyledButton'
-import {StyledFancyButton, StyledThemeButton} from './StyledPropsButton'
+/** @format */
+
+import React from "react";
+import StyledSuccessButton from "./StyledButton";
+import { StyledFancyButton, StyledThemeButton } from "./StyledPropsButton";
 
 const InitComponent = () => {
   return (
     <div>
-     {/* styled button  */}
-     <StyledSuccessButton onClick={() => console.log("btn clicked")}>
-     Basic button
-     </StyledSuccessButton>
+      {/* styled button  */}
+      <StyledSuccessButton onClick={() => console.log("btn clicked")}>
+        Basic button
+      </StyledSuccessButton>
 
-     {/* usage of props */}
-     {/* <StyledPropsButton varient='red'>I am using Properties</StyledPropsButton> */}
+      {/* usage of props */}
+      {/* <StyledPropsButton varient='red'>I am using Properties</StyledPropsButton> */}
 
-     {/* Extending styles   as="div" */}
-     <StyledFancyButton  varient='red'>I am using Properties</StyledFancyButton>
+      {/* Extending styles   as="div" */}
+      <StyledFancyButton varient='red'>I am using Properties</StyledFancyButton>
 
-{/* animation  */}
-     <br />
-     <br />
-     <StyledThemeButton>Theme Button</StyledThemeButton>
-     </div>
-  )
-}
+      {/* animation  */}
+      <br />
+      <br />
+      <StyledThemeButton>Theme Button</StyledThemeButton>
+    </div>
+  );
+};
 
-export default InitComponent
+export default InitComponent;
